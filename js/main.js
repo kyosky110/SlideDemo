2482e4d6f3a68819bb4d440921aec696078cca64
kyosky110 <493203254@qq.com>
2018-05-09 17:23:12 +0800

    无缝轮播

--- js/main.js
+++ js/main.js
@@ -1,13 +1,102 @@
 
-var allButtons = $('#sildeButtions > button')
-buttonlistener()
 
-//自动轮播
-//1 计时器 3000 2 到时间点一个 jq点击是 $().trigger('click')
-var indexButton = 0
-var buttonLength = allButtons.length
-$(allButtons[indexButton]).trigger('click')
-var timerId = setTimer()
+let indexPage = 1
+let pageLength = $('#slideImgs > img').length
+let allButtons = $('#sildeButtions > button')
+if(pageLength !== allButtons.length){
+    alert('按钮和图片数量不一致哦')
+}
+
+init()
+buttonlistener()
+let timerId = setTimer()
+
+//下面是方法
+
+function init() {
+    setCurrentPage(getCurrentImg())
+        .siblings().addClass('enterPage')
+    activeButton($(allButtons[getCurrentIndex() - 1]))
+}
+
+/**
+ * 设置定时器
+ */
+function setTimer() {
+    return setInterval(() => {
+        console.log(getButtonCurrentIndex())
+        changePage(getCurrentIndex(),$(allButtons[getButtonCurrentIndex()]))
+        //退出
+        // setLeavePage(getCurrentImg()).one('transitionend', event => {
+        //     setEnterPage($(event.currentTarget))
+        // })
+        // indexPage++
+        // //进入
+        // activeButton($(allButtons[getCurrentIndex() - 1]))
+        // setCurrentPage(getCurrentImg())
+    }, 3000)
+}
+
+function getCurrentIndex() {
+    return indexPage > pageLength ? (indexPage = 1) : indexPage
+}
+
+function getButtonCurrentIndex(){
+    return getCurrentIndex() >= pageLength ? 0 : getCurrentIndex()
+}
+
+function getCurrentImg() {
+    return $(`#slideImgs > img:nth-child(${getCurrentIndex()})`)
+}
+
+function setLeavePage($img) {
+    return $img.removeClass('currentPage').addClass('leavePage')
+}
+
+function setEnterPage($img) {
+    if($img.hasClass('currentPage')){
+        return
+    }
+    return $img.removeClass('leavePage').addClass('enterPage')
+}
+
+function setCurrentPage($img) {
+    return $img.removeClass('enterPage').removeClass('leavePage').addClass('currentPage')
+}
+
+function activeButton($button) {
+    $button.addClass('buttonActive')
+        .siblings('.buttonActive').removeClass('buttonActive')
+}
+
+function changePage(lastIndex, $button) {
+    activeButton($button)
+    //退出
+    setLeavePage(getCurrentImg()).one('transitionend', event => {
+        setEnterPage($(event.currentTarget))
+    })
+    indexPage = lastIndex + 1
+    //进入
+    setCurrentPage(getCurrentImg())
+}
+
+function buttonlistener() {
+
+    for (let i = 0; i < allButtons.length; i++) {
+        $(allButtons[i]).on('click', function (event) {
+            window.clearInterval(timerId)
+            changePage($(event.currentTarget).index(),$(event.currentTarget))
+            timerId = setTimer()
+            // var index = $(event.currentTarget).index()
+            // activeButton($(event.currentTarget))
+            // setLeavePage(getCurrentImg()).one('transitionend', event => {
+            //     setEnterPage($(event.currentTarget))
+            // })
+            // indexPage = index + 1
+            // setCurrentPage(getCurrentImg())
+        })
+    }
+}
 
 //修复鼠标放在上面停止滑动的bug
 //简单鼠标进入和出去的事件 mouseenter mouseleave
@@ -19,33 +108,3 @@ $('#slideWindow').on('mouseenter',()=>{
 $('#slideWindow').on('mouseleave',()=>{
     timerId = setTimer()
 })
-
-function setTimer(){
-    return setInterval(()=>{
-        indexButton++
-        $(allButtons[indexButton%buttonLength]).trigger('click')
-    },3000)
-}
-
-function activeButton($button){
-    $button.addClass('red')
-        .siblings('.red').removeClass('red')
-}
-
-function buttonlistener(){
-
-    for (let i = 0; i < allButtons.length; i++) {
-        $(allButtons[i]).on('click',function(event){
-            var index = $(event.currentTarget).index()
-            var distance = index * -300
-            $('#slideImgs').css({
-                transform:'translate('+ distance + 'px)'
-            })
-            // $(event.currentTarget).addClass('red')
-            // .siblings('.red').removeClass('red')
-            activeButton($(event.currentTarget))
-            indexButton = index
-        })
-        
-    }
-}
